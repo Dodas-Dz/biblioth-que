c2a86162edb6f7e570e9cf889386d834bdfa9ed7
zakaria beloufa <zakaria.beloufa13@gmail.com>
2021-11-11 15:41:53 +0000

    v2.22

---
+++ resources/views/layouts/FooterU.blade.php
@@ -0,0 +1,88 @@
+<section class="bg-secondary">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-lg-6 mb-4 order-0 order-sm-0"><a class="text-decoration-none" href="#"><img src="assetsU/img/gallery/footer-logo.png" height="51" alt="" /></a>
+                <p class="text-light my-4"> <i class="fas fa-map-marker-alt me-3"></i><span class="text-light">1500 Treat Ave, Suite 200 &nbsp;</span><a class="text-light" href="tel:+604-680-9785">+604-680-9785</a><br />San Francisco, CA 94110</p>
+                <p class="text-light"> <i class="fas fa-envelope me-3"> </i><a class="text-light" href="mailto:vctung@outlook.com">Knowledgepark13@gmail.com </a></p>
+                <p class="text-light"> <i class="fas fa-phone-alt me-3"></i><a class="text-light" href="tel:1-800-800-2299">+213 777 22 5736 (Support)</a></p>
+            </div>
+            <div class="col-6 col-sm-4 col-lg-2 mb-3 order-2 order-sm-1">
+                <h5 class="lh-lg fw-bold mb-4 text-light font-sans-serif">Community </h5>
+                <ul class="list-unstyled mb-md-4 mb-lg-0">
+                    <li class="lh-lg"><a class="text-200" href="#!">Learners</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Partners</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Developers</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Beta Testers</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Translators</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Blog</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Tech Blog</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Teaching Center</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-sm-4 col-lg-2 mb-3 order-3 order-sm-2">
+                <h5 class="lh-lg fw-bold text-light mb-4 font-sans-serif">Informations</h5>
+                <ul class="list-unstyled mb-md-4 mb-lg-0">
+                    <li class="lh-lg"><a class="text-200" href="#!">About</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Pricing</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Blog</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Careers</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-sm-4 col-lg-2 mb-3 order-3 order-sm-2">
+                <h5 class="lh-lg fw-bold text-light mb-4 font-sans-serif"> More</h5>
+                <ul class="list-unstyled mb-md-4 mb-lg-0">
+                    <li class="lh-lg"><a class="text-200" href="#!">Press</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Investors</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Terms</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Privacy</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Help</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Accessibility</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Contact</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Articles</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Directory</a></li>
+                    <li class="lh-lg"><a class="text-200" href="#!">Affiliates</a></li>
+                </ul>
+            </div>
+
+        </div>
+        <div class="my-4 col row">
+            <div class="col-md-10">
+                <p class="text-white fs-0 my-2 fw-bold ">All rights Reserved &copy; Knowledge Park, 2021</p>
+            </div>
+            <div class=" col-md-2 ">
+                <img class="img-responsive " src="assetsU\img\favicons\Logo-Univ_Tlemcen.png" height="80" alt="logo" />
+            </div>
+        </div>
+    </div>
+    <!-- end of .container-->
+
+</section>
+<!-- <section> close ============================-->
+<!-- ============================================-->
+
+
+
+</main>
+<!-- ===============================================-->
+<!--    End of Main Content-->
+<!-- ===============================================-->
+
+
+
+
+<!-- ===============================================-->
+<!--    JavaScripts-->
+<!-- ===============================================-->
+<script src="assetsU/vendors/@popperjs/popper.min.js"></script>
+<script src="assetsU/vendors/bootstrap/bootstrap.min.js"></script>
+<script src="assetsU/vendors/is/is.min.js"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+<script src="assetsU/vendors/fontawesome/all.min.js"></script>
+
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="assetsU/js/theme.js"></script>
+
+<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&amp;family=Rubik:wght@300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
