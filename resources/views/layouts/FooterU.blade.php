c617f4c8bb51d03706712a1586c1c9260bab0480
zakaria beloufa <zakaria.beloufa13@gmail.com>
2021-11-12 15:51:49 +0000

    2.5

--- resources/views/layouts/FooterU.blade.php
+++ resources/views/layouts/FooterU.blade.php
@@ -1,13 +1,13 @@
-<section class="bg-secondary mb-n20" >
+<section class="bg-secondary mb-n20">
 
     <div class="container">
         <div class="row">
             <div class="col-12 col-sm-12 col-lg-6 mb-4 order-0 order-sm-0">
                 <div class="container"><a class="navbar-brand" href="index.html"><img src="assets/img/favicon.png" height="100" alt="logo" /></a><a class="text-decoration-none mr-150" href="#">
-</div>
-                <br class="text-light my-4"> <i class="fas fa-map-marker-alt me-3" style="color:white"></i><span class="text-light">Nouveuau pôle la rocade  &nbsp;</br></br> &nbsp; Tlemcen, 13 000</span>
-                <p class="text-light"> </br></br><i class="fas fa-envelope me-3"> </i><a class="text-light" >maktaba@gmail.com </a></p>
-                <p class="text-light"> <i class="fas fa-phone-alt me-3"></i><a class="text-light">Fix: 043 41 11 95 </br></br>&nbsp; &nbsp;Fax: 043 41 11 91</br></br>  &nbsp;&nbsp;Télex: 1871-18034</a></p>
+                </div>
+                <br class="text-light my-4"> <i class="fas fa-map-marker-alt me-3" style="color:white"></i><span class="text-light">Nouveuau pôle la rocade &nbsp;</br></br> &nbsp; Tlemcen, 13 000</span>
+                <p class="text-light"> </br></br><i class="fas fa-envelope me-3"> </i><a class="text-light">maktaba@gmail.com </a></p>
+                <p class="text-light"> <i class="fas fa-phone-alt me-3"></i><a class="text-light">Fix: 043 41 11 95 </br></br>&nbsp; &nbsp;Fax: 043 41 11 91</br></br> &nbsp;&nbsp;Télex: 1871-18034</a></p>
             </div>
             <div class="col-6 col-sm-4 col-lg-2 mb-3 order-3 order-sm-2">
                 <h5 class="lh-lg fw-bold text-light mb-4 font-sans-serif">Menu</h5>
@@ -16,7 +16,7 @@
                     <li class="lh-lg"><a class="text-200" href="#!">Livres</a></li>
                     <li class="lh-lg"><a class="text-200" href="#!">A propos</a></li>
                     <li class="lh-lg"><a class="text-200" href="#!">Mes livres</a></li>
-                   
+
                 </ul>
             </div>
             <div class="col-6 col-sm-4 col-lg-2 mb-3 order-2 order-sm-1">
@@ -33,39 +33,39 @@
                     <li class="lh-lg"><a class="text-200" href="#!">Documentaire</a></li>
                 </ul>
             </div>
-           
+
             <div class="col-6 col-sm-4 col-lg-2 mb-3 order-3 order-sm-2">
                 <h5 class="lh-lg fw-bold text-light mb-4 font-sans-serif"> Réseaux sociaux</h5>
                 <ul class="list-unstyled mb-9 md-7 mb-lg-0">
-                   <li  class="lh-lg">
-                       <i class="fw-bold fs-5 fab fa-facebook " style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
-                       <i class="fw-bold fs-5 fab fa-instagram" style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
-                       <i class="fw-bold fs-5 fab fa-twitter" style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
+                    <li class="lh-lg">
+                        <i class="fw-bold fs-5 fab fa-facebook " style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
+                        <i class="fw-bold fs-5 fab fa-instagram" style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
+                        <i class="fw-bold fs-5 fab fa-twitter" style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
                     </li>
-                    </br> 
-                    <li   class="lh-lg"><i class="fw-bold fs-5 fab fa-whatsapp" style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
-                       <i class="fw-bold fs-5 fab fa-youtube" style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
-                       <i class="fw-bold fs-5 fab fa-linkedin" style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
+                    </br>
+                    <li class="lh-lg"><i class="fw-bold fs-5 fab fa-whatsapp" style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
+                        <i class="fw-bold fs-5 fab fa-youtube" style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
+                        <i class="fw-bold fs-5 fab fa-linkedin" style="color:white"></i><a class="text-200" href="#!"></a>&nbsp; &nbsp;
                     </li>
-                  
+
                 </ul>
             </div>
 
         </div>
 
 
-        
-        <div >
+
+        <div>
 
 
-        <br class="text-light my-4"> 
-        <div class="container"><a class="navbar-brand" href="index.html">
-            <img src="assetsU\img\favicons\Logo-Univ_Tlemcen.png" height="55" alt="logo" /></a>
-        <span class="text-light">All rights Reserved &copy; univ-Tlemcen, 2021</span>
+            <br class="text-light my-4">
+            <div class="container"><a class="navbar-brand" href="index.html">
+                    <img src="assetsU\img\favicons\Logo-Univ_Tlemcen.png" height="55" alt="logo" /></a>
+                <span class="text-light">All rights Reserved &copy; univ-Tlemcen, 2021</span>
 
-        </div>
-         
-    <!-- end of .container-->
+            </div>
+
+            <!-- end of .container-->
 
 </section>
 <!-- <section> close ============================-->
@@ -93,5 +93,10 @@
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <script src="assetsU/js/theme.js"></script>
+<script src="assetsU/js/owl-carousel.js"></script>
+<script src="assetsU/js/animation.js"></script>
+<script src="assetsU/js/imagesloaded.js"></script>
+<script src="assetsU/js/custom.js"></script>
+
 
 <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&amp;family=Rubik:wght@300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
